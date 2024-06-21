9614cc52e0606892a34f7d8ebd650548f5ca70ab
pamongsky <160507696+pamongsky@users.noreply.github.com>
2024-06-21 14:42:18 +0700

    Add files via upload

---
+++ PRAKTIKUM 4/Poligon.java
@@ -0,0 +1,12 @@
+/* Nama File : Poligon.java */
+/* Nama / NIM : Muhammad Fahmi / 24060122140111 */
+
+package org.poligon;
+
+public class Poligon {
+    protected int jumlahSisi;
+
+    public int getJumlahSisi(){
+        return this.jumlahSisi;
+    }
+}
---
+++ PRAKTIKUM 4/Segitiga.java
@@ -0,0 +1,25 @@
+/* Nama File : PersegiPanjang.java */
+/* Deskripsi : representasi dari objek segitiga, turunan kelas poligon */
+/* Nama / NIM : Muhammad Fahmi / 24060122140111 */
+
+package org.bangundatar;
+
+import org.poligon.Poligon;
+
+public class Segitiga extends Poligon{
+    private double alas, tinggi;
+
+    public Segitiga(double alas, double tinggi, int jumlahSisi){
+        this.alas = alas;
+        this.tinggi = tinggi;
+        this.jumlahSisi = jumlahSisi;
+    }
+
+    public double hitungLuas(){
+        return 0.5 * alas * tinggi;
+    }
+
+    public void printInfo(){
+        System.out.println("Bangun Segitiga bersisi " + this.getJumlahSisi());
+    }
+}
